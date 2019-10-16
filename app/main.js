48fe3e208b0abe68074595aa595b43fa6fd44ef7
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-10-16 16:20:29 +0300

    Registration namespace of 'BattleCityGame.GameEngine'

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 const img = 'static/img'
 const data = 'static/data'
 
-const { GameEngine } = BattleCityGame
+const { GameEngine } = BattleCityGame // выгружаем свойство GameEngine из объекта BattleCityGame
 const { Loader } = GameEngine // записываем свойство Loader (класс) объекта GameEngine в константу
 
 const loader = new Loader // создаем экземпл€р класса Loader

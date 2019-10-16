eaf90f1a337990bd93c0edb65412c0483d0c784f
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-10-17 01:28:23 +0300

    Change method update and variable sprite

--- app/_init.js
+++ app/_init.js
@@ -1,4 +1,20 @@
 const { GameEngine } = BattleCityGame // выгружаем свойство GameEngine из объекта BattleCityGame
-const { Loader } = GameEngine // записываем свойство Loader (класс) объекта GameEngine в константу
+const { Container, Loader, Renderer, Sprite } = GameEngine // записываем свойство Loader (класс) объекта GameEngine в константу
 
 const loader = new Loader // создаем экземпл€р класса Loader
+
+let sprites = [] // значение по умолчанию дл€ переменной спрайта
+
+const renderer = new Renderer({
+    width: 500,
+    height: 500,
+    background: 'gray',
+
+    update(timestamp) {
+        if (!sprites) return
+        for (const sprite of sprites) {
+            sprite.update(timestamp)
+        }
+        
+    }
+})

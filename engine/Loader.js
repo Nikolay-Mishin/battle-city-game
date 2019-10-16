1da883e47606ecedd9736d4e47a1027a46b1d3c5
Николай Мишин <mishin.nikolay.d3@mail.ru>
2019-10-16 16:18:33 +0300

    Registration namespace of 'BattleCityGame.GameEngine'

--- engine/Loader.js
+++ engine/Loader.js
@@ -137,7 +137,7 @@
 
     // если объект GameEngine инициализирован (существует) берем его значение, иначе создаем пустой объект (инициализируем)
     namespace.set('BattleCityGame.GameEngine'); // регистрируем объект GameEngine в объекте window
-    const { GameEngine } = BattleCityGame
+    const { GameEngine } = BattleCityGame // выгружаем свойство GameEngine из объекта BattleCityGame
     GameEngine.Loader = Loader // регистрируем класс Loader в объекте GameEngine
     // window.GameEngine = window.GameEngine || {} // регистрируем объект GameEngine в объекте window
     // GameEngine.Loader = Loader // регистрируем класс Loader в объекте GameEngine
